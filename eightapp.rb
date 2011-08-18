4e7f57c2570a3fa80a7b3aabb4b6b60c5de44cbe
Jason Lagaac <jason.lagaac@me.com>
2011-08-18 15:02:42 +0800

    Added functionality for question post feedback

--- eightapp.rb
+++ eightapp.rb
@@ -6,6 +6,8 @@ require "dm-timestamps"
 require "dm-migrations"
 
 configure :development do
+  set :sessions, true
+  set :test_uid,  1
   DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/eightapp.db")
 end
 
@@ -33,8 +35,10 @@ end
 
 class Answer
   include DataMapper::Resource
+  
+  property :id,   Serial
   property :answered_question_id,   Integer
-
+  
   belongs_to :user  
 end
 
@@ -56,28 +60,28 @@ get '/ask' do
 end
 
 post '/ask' do
-  if params [:ask][:question_txt].empty?
+  if params[:post][:question_txt].empty?
 	    redirect '/ask'
 	else
-		@question = Question.new(params[:ask]) 
-     
+		@question = Question.new(params[:post])
+    @question.user_id = 1
+
     if @question.save
-      redirect "/question/#{@question.id}" 
+      redirect "/question_posted" 
     else
-      redirect 'ask'
+      redirect '/ask'
     end 
 	end
 end
 
 
 # View Questions and answer Yes or No
-
 get '/question/:id' do 
   @question = Question.get(params[:id])
   if @question
     erb :show
   else
-    redirect('/list')
+    redirect '/list'
   end
 end
 
@@ -90,20 +94,33 @@ post '/question/:id' do
     elsif (params[:post][:answer] == 'no')
       @question.no += 1 
     else
-      redirect('question/:id')
+      redirect 'question/:id'
+    end
   else
-    redirect('list')
+    redirect 'list'
   end
 end
 
 # View a list of unanswered questions
 get '/list' do
-
+  # Obtain a list of unanswered question by doing a 
+  # join of the Answers table and the Questions table
+  # and also excluding a user's own questions.
+  @unanswered_questions = repository(:default).adapter.query(
+    "SELECT questions.id FROM questions 
+      LEFT OUTER JOIN answers ON questions.id = answers.answered_question_id
+      WHERE answers.answered_question_id is null and questions.user_id != #{settings.test_uid}"
+    )
+  @unanswered_questions.first.to_s
 end
 
 
 # A Simple page detailing what page redirect
 # which says that the question was posted
 get '/question_posted' do
+  @question = Question.last(:user_id => settings.test_uid)
 
+  # Obtain the latest question from a specific user
+  # and then post "You're question blah has been posted"
+  @question.question_txt
 end
